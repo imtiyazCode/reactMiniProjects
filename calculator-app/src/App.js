5665bf9abf823bd1c31799d229a3982634dd40e0
Imtiyaz <76092300+IntiTalent@users.noreply.github.com>
2023-02-03 12:10:21 +0530

    functionality implemented

--- calculator-app/src/App.js
+++ calculator-app/src/App.js
@@ -1,10 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [input, setInput] = useState('');
+  const handleClick = (value) => {
+    setInput(input + value);
+  };
+
+  const handleEqual = () => {
+    setInput(eval(input).toString());
+  };
+
+  const handleClear = () => {
+    setInput("");
+  };
   return (
-    <div className="App">
-      <p>Hello world</p>
+    <div className="calculator">
+      <input className="display" type="text" value={input} readOnly />
+      <div className="keypad">
+        <button className='num' onClick={() => handleClick("7")}>7</button>
+        <button className='num' onClick={() => handleClick("8")}>8</button>
+        <button className='num' onClick={() => handleClick("9")}>9</button>
+        <button className='operator' onClick={() => handleClick("/")}>/</button>
+        <br />
+        <button className='num' onClick={() => handleClick("4")}>4</button>
+        <button className='num' onClick={() => handleClick("5")}>5</button>
+        <button className='num' onClick={() => handleClick("6")}>6</button>
+        <button  className='operator'onClick={() => handleClick("*")}>*</button>
+        <br />
+        <button className='num' onClick={() => handleClick("1")}>1</button>
+        <button className='num' onClick={() => handleClick("2")}>2</button>
+        <button className='num' onClick={() => handleClick("3")}>3</button>
+        <button  className='operator'onClick={() => handleClick("-")}>-</button>
+        <br />
+        <button className='num' onClick={() => handleClick("0")}>0</button>
+        <button className='clear' onClick={handleEqual}>=</button>
+        <button className='clear' onClick={handleClear}>C</button>
+        <button className='operator'onClick={() => handleClick("+")}>+</button>
+      </div>
     </div>
   );
 }
