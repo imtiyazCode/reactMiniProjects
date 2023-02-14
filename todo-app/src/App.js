30a8797ab538e0d70b95d1d17bf40ae9df20a6a9
Imtiyaz <76092300+IntiTalent@users.noreply.github.com>
2023-02-14 23:54:07 +0530

    implementing some basic functionality of todoapp

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,9 +1,46 @@
 import './App.css';
+import { useState } from 'react';
+import Task from './Components/Task';
 
 function App() {
+  const [title, setTitle] = useState('')
+  const [tasks, setTasks] = useState([
+    {
+      title: "Follow Codeclash",
+      completed: true
+    },
+    {
+      title: "Do your workout",
+      completed: true
+    },
+    {
+      title: "Hangout with friends",
+      completed: false
+    }
+  ]);
+
+  const addTask = (e) => {
+    e.preventDefault();
+    setTasks([...tasks, { title, completed: false }])
+    setTitle("")
+  }
+
+  const handleChange = (e) => {
+    setTitle(e.target.value)
+  }
+
   return (
-    <div className="App">
-      <h1>Hello world</h1>
+    <div className="todo-container">
+      <div className="header">TODO - ITEMS</div>
+      <form action="addTask">
+        <input type="text" name="task" value={title} onChange={handleChange} />
+        <button type="submit" onClick={addTask}>Add Task</button>
+      </form>
+      <div className="tasks">
+        {tasks.map((task, index) => (
+          <Task tasks={tasks} setTasks={setTasks} task={task} key={index} index={index} />
+        ))}
+      </div>
     </div>
   );
 }
