8d1a6133419f24618c8ac6bd18395fba041dcc96
Imtiyaz <76092300+IntiTalent@users.noreply.github.com>
2023-02-25 13:21:44 +0530

    changing file format

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,62 +1,33 @@
 import './App.css';
 import { useState, useEffect } from 'react';
-import Task from './Components/Task';
 import Switch from "react-switch";
 import { BsSunFill, BsFillMoonStarsFill } from 'react-icons/bs'
+import TaskContainer from './Components/TaskContainer';
 
 function App() {
 
-  const [todo, setTodo] = useState({ completed: false, title: "", description: "" })
-  const [dark, setDark] = useState(true);
   const [tasks, setTasks] = useState([])
-
+  const [dark, setDark] = useState(true);
+  
   useEffect(() => {
     let myTodo = localStorage.getItem('myTodoTasks');
-    if(myTodo){
-      setTasks(JSON.parse(myTodo))
+    if (myTodo) {
+        setTasks(JSON.parse(myTodo))
     }
-  }, [])
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (todo.title) {
-      let newTask = { ...todo }
-      let newTasks = [...tasks, newTask]
-      setTasks(newTasks)
-      setTodo({ title: '', description: '', completed: false })
-      localStorage.setItem("myTodoTasks", JSON.stringify(newTasks));
-    }
-  }
-  
-  const handleChange = (e) => {
-    setTodo({ ...todo, [e.target.name]: e.target.value })
-  }
+}, [])
 
   return (
     <div className={`${dark ? 'darkMode-App' : "lightMode-App"} App`}>
-      <h1 className='app-title'>ToDo App</h1>
+      <div className={`${dark ? 'darkMode-app-title-container' : "lightMode-app-title-container"} app-title-container`}>
+        <h1 className='app-title'>ToDo App</h1>
+      </div>
       <Switch
         checked={dark}
         onChange={() => setDark(!dark)}
         uncheckedIcon={<div className='check-sun-btn' ><BsSunFill size={18} /></div>}
         checkedIcon={<div className='check-moon-btn'><BsFillMoonStarsFill size={18} /></div>} />
 
-      <form className='input-form' action='handleSubmit' onSubmit={handleSubmit}>
-        <input className='task-input task-input-title' 
-              name='title' type="text" placeholder='Enter Title' 
-              onChange={handleChange} value={todo.title} />
-        <input className='task-input task-input-desc' 
-              name='description' type="text" placeholder='Enter Description' 
-              onChange={handleChange} value={todo.description} />
-        <button className={`task-btn ${dark ? 'darkMode-add-btn' : 'lightMode-add-btn'} add-btn`} 
-              type='submit'>Add</button>
-      </form >
-
-      <div className={`${dark ? 'darkMode-box-tasks-container' : "lightMode-box-tasks-container"} box-tasks-container`}>
-        {tasks?.map((task, i) => {    
-          return <Task task={task} tasks={tasks} setTasks={setTasks} index={i} dark={dark} key={i} />
-        })}
-      </div>
+      <TaskContainer tasks={tasks} setTasks={setTasks} dark={dark} />
     </div >
   );
 }
---
+++ todo-app/src/Components/TaskContainer/index.js
@@ -0,0 +1,46 @@
+import React, {useState} from 'react'
+import Task from '../Task'
+import './TaskContainer.css'
+
+const TaskContainer = ({tasks, setTasks, dark}) => {
+    
+  const [todo, setTodo] = useState({ completed: false, title: "", description: "" })
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (todo.title) {
+            let newTask = { ...todo }
+            let newTasks = [...tasks, newTask]
+            setTasks(newTasks)
+            setTodo({ title: '', description: '', completed: false })
+            localStorage.setItem("myTodoTasks", JSON.stringify(newTasks));
+        }
+    }
+
+    const handleChange = (e) => {
+        setTodo({ ...todo, [e.target.name]: e.target.value })
+    }
+
+    return (
+        <div className='tasks-container'>
+            <form className='input-form' action='handleSubmit' onSubmit={handleSubmit}>
+                <input className='task-input task-input-title'
+                    name='title' type="text" placeholder='Enter Title'
+                    onChange={handleChange} value={todo.title} />
+                <input className='task-input task-input-desc'
+                    name='description' type="text" placeholder='Enter Description'
+                    onChange={handleChange} value={todo.description} />
+                <button className={`task-btn ${dark ? 'darkMode-add-btn' : 'lightMode-add-btn'} add-btn`}
+                    type='submit'>Add</button>
+            </form >
+
+            <div className={`${dark ? 'darkMode-box-tasks-container' : "lightMode-box-tasks-container"} box-tasks-container`}>
+                {tasks?.map((task, i) => {
+                    return <Task task={task} tasks={tasks} setTasks={setTasks} index={i} dark={dark} key={i} />
+                })}
+            </div>
+        </div>
+    )
+}
+
+export default TaskContainer
