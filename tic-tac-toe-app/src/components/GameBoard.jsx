d3db7beb90649f124884035d589accae8988b792
Imtiyaz <76092300+IntiTalent@users.noreply.github.com>
2023-06-17 23:42:56 +0530

    tic-tac-toe first first version is completed

---
+++ tic-tac-toe-app/src/components/GameBoard.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const GameBoard = ({ winner, currentPlayer, winningCells, handleCellClick, resetGame, board }) => {
+    return (
+        <div className='game-board'>
+            {winner ? winner === "draw" ? (
+                <p className='winner-title'>It's a draw!</p>
+            ) : (
+                <p className='winner-title'>Player <span className={winner}>{winner}</span> wins!</p>
+            ) : <p className={`winner-title`}>Current Player: <span className={currentPlayer}>{currentPlayer}</span></p>}
+            <div className="board">
+                {board.map((cell, index) => (
+                    <div
+                        key={index}
+                        className={`cell ${winningCells.includes(index) ? 'winning' : ''} ${cell}`}
+                        onClick={() => handleCellClick(index)}
+                    >
+                        {cell}
+                    </div>
+                ))}
+            </div>
+            {winner && (
+                <div className="message">
+                    <button onClick={resetGame}>Play Again</button>
+                </div>
+            )}</div>
+    )
+}
+
+export default GameBoard
