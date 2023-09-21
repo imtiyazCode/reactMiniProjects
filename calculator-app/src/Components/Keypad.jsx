b372ca1bc2330a38fbe1d0caca03d6badfe087a2
Imtiyaz <76092300+IntiTalent@users.noreply.github.com>
2023-09-21 22:45:17 +0530

    update calculator app keypad css

--- calculator-app/src/Components/Keypad.jsx
+++ calculator-app/src/Components/Keypad.jsx
@@ -1,45 +1,29 @@
-import React from 'react'
+import React from 'react';
 
 const Keypad = ({ handleClick, handleClear, handleEqual }) => {
-    return (
-        <div className="keypad">
-            <button className='digit-keys'
-                onClick={() => handleClick("7")}>7</button>
-            <button className='digit-keys'
-                onClick={() => handleClick("8")}>8</button>
-            <button className='digit-keys'
-                onClick={() => handleClick("9")}>9</button>
-            <button className='operator-keys'
-                onClick={() => handleClick("/")}>/</button>
-            <br />
-            <button className='digit-keys'
-                onClick={() => handleClick("4")}>4</button>
-            <button className='digit-keys'
-                onClick={() => handleClick("5")}>5</button>
-            <button className='digit-keys'
-                onClick={() => handleClick("6")}>6</button>
-            <button className='operator-keys'
-                onClick={() => handleClick("*")}>*</button>
-            <br />
-            <button className='digit-keys'
-                onClick={() => handleClick("1")}>1</button>
-            <button className='digit-keys'
-                onClick={() => handleClick("2")}>2</button>
-            <button className='digit-keys'
-                onClick={() => handleClick("3")}>3</button>
-            <button className='operator-keys'
-                onClick={() => handleClick("-")}>-</button>
-            <br />
-            <button className='digit-keys bottom-left-corner'
-                onClick={() => handleClick("0")}>0</button>
-            <button className='fun-keys'
-                onClick={handleEqual}>=</button>
-            <button className='fun-keys'
-                onClick={handleClear}>C</button>
-            <button className='operator-keys bottom-right-corner'
-                onClick={() => handleClick("+")}>+</button>
-        </div>
-    )
+  const buttonConfigurations = [
+    '7', '8', '9', '/',
+    '4', '5', '6', '*',
+    '1', '2', '3', '-',
+    '0', '=', 'C', '+'
+  ];
+
+  return (
+    <div className="keypad">
+      {buttonConfigurations.map((label, index) => (
+        <button
+          key={index}
+          className={`${
+            ['/', '*', '-', '+'].includes(label) ? 'operator-keys' :
+            ['=', 'C'].includes(label) ? 'fun-keys' : 'digit-keys'
+          } ${index === 12 ? 'bottom-left-corner' : index === 14 ? 'bottom-right-corner' : ''}`}
+          onClick={() => label === '=' ? handleEqual() : label === 'C' ? handleClear() : handleClick(label)}
+        >
+          {label}
+        </button>
+      ))}
+    </div>
+  );
 }
 
-export default Keypad
+export default Keypad;
